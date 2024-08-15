dd640587593e24c0fffdfc35acba782d8ec1e42e
Sakib-2004043 <sakib1514817122@gmail.com>
2024-08-16 03:47:17 +0600

    3.47

---
+++ src/components/EventsFunctionsState.jsx
@@ -0,0 +1,27 @@
+
+import React, { useState } from 'react'
+
+const EventsFunctionsState = () => {
+
+  const [name, setName] = useState("Sakibul")
+  const sayHello = (name) => {
+    alert("Hello "+name)
+  }
+  const changeName = () => {
+    setName(name === "Sakibul"?"Hasan":"Sakibul")
+  }
+  return (
+    <>
+      <h2>Events Functions And State</h2>
+      <button onClick={() => alert("Clicked")}>
+        Click Me
+      </button>
+      <br /><br />
+      <button onClick={() => sayHello("Sakib")}>Hellow</button>
+      <br /><br />
+      <button onClick={() => changeName("Sakibul")}>{name}</button>
+    </>
+  )
+}
+
+export default EventsFunctionsState
