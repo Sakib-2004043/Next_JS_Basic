efd1b7db31a414317c72a840a480e90dc8c19c4a
Sakib-2004043 <sakib1514817122@gmail.com>
2024-08-16 04:11:27 +0600

    4.11

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,20 @@
-"use client"
+import Link from "next/link";
 import styles from "./page.module.css";
-import EventsFunctionsState from "@/components/EventsFunctionsState";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Next JS Basic</h1>
-      <hr/>
-      <EventsFunctionsState/>
+      <br/>
+      <div className="list-div">
+        <ol>
+          <li><Link href="/home">Home</Link></li>
+          <li><Link href="/components">components</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
+          <li><Link href="/about">About</Link></li>
+        </ol>
+      </div>
+      
     </main>
   );
 }
