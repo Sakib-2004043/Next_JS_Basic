a8094d1eec6ca6a5a72fca61cc5e3e398f849c87
Sakib-2004043 <sakib1514817122@gmail.com>
2024-09-22 16:37:42 +0600

    correction DB

--- src/app/Database/connect.js
+++ src/app/Database/connect.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
+const DB_URL = process.env.connectionString
 
 const connectDB = async () => {
   if (mongoose.connection.readyState !== 1) {
     try {
-      await mongoose.connect(process.env.connectionString);
+      await mongoose.connect(DB_URL);
       console.log("Connected to MongoDB Atlas");
     } catch (error) {
       console.error("Error connecting to MongoDB:", error);
